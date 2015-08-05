5efee78f2923b9662d7953bafd24df88c641c51b
fschabmeyer <fschabmeyer@googlemail.com>
2015-08-05 21:14:57 +0200

    Update setup-php-extensions.sh

--- travis/setup-php-extensions.sh
+++ travis/setup-php-extensions.sh
@@ -5,8 +5,7 @@ set -x
 
 function find_php_ini
 {
-#	echo $(php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||")
-echo $(php --ini | grep "Loaded Configuration")
+	echo $(php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||")
 }
 
 php_ini_file=$(find_php_ini)

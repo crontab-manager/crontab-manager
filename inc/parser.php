18c12befbb0f3c4dd759aee13bd0fa24182a055f
Felix Schabmeyer <fschabmeyer@googlemail.com>
2015-08-12 19:00:55 +0200

    Test Code Climate #2

--- inc/parser.php
+++ inc/parser.php
@@ -11,7 +11,7 @@ class parser {
      *
      * @return array
      */
-    public function getIniSettings(){
+    private function getIniSettings(){
         $filename = "config.ini";
         $array = parse_ini_file($filename);
         $this->ServerToTest = $array;
@@ -21,7 +21,7 @@ class parser {
      * @param $serveriptotest
      * @return resource
      */
-    public function getsshConnection($serveriptotest){
+    private function getsshConnection($serveriptotest){
         $connection = ssh2_connect($serveriptotest, 22);
         ssh2_auth_password($connection, 'root', 'test');
         return $connection;
@@ -32,7 +32,7 @@ class parser {
      * @param $commandtotest
      * @return string
      */
-    public function getsshStreamData($connection, $commandtotest){
+    private function getsshStreamData($connection, $commandtotest){
         $stream = ssh2_exec($connection, "tail /var/spool/cron/crontabs/root");
         stream_set_blocking($stream, true);
         $data = "";
@@ -46,7 +46,7 @@ class parser {
     /**
      *
      */
-    public function getCrontabFromRemoteServer(){
+    private function getCrontabFromRemoteServer(){
         foreach ($this->ServerToTest["serverip"] as $serveriptotest) {
             $connection = $this->getsshConnection($serveriptotest);
             $data = $this->getsshStreamData($connection,"tail /var/spool/cron/crontabs/root");
